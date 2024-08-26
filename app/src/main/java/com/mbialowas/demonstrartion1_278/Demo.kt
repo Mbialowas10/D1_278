e4d64025744385ad4c21a25e80ff403af79126e8
Mike bialowas <mikebialowas@videoedit-s.academic.rrc.ca>
2024-08-26 09:33:34 -0500

    Initial commit

---
+++ app/src/main/java/com/mbialowas/demonstrartion1_278/Demo.kt
@@ -0,0 +1,37 @@
+package com.mbialowas.demonstrartion1_278
+
+fun main(){
+
+    // single line comment
+
+    /*
+    multi line comment
+     */
+
+    println("Hello World")
+
+    // variable declaration
+    val num = 10 // read  only variable
+
+    var num2 = 33 // writable variable aka mutable
+
+    /*
+    DATA TYPES  -
+    Primitives
+        Integer, Double, Boolean
+   Non-primitive
+    String, Array Classes
+     */
+    val multiLineString = """
+            This is line 1
+            This is line 2
+            This is line 3
+        """
+    print(multiLineString)
+
+    val PI =3.1452
+    println(PI)
+
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Demonstrartion1_278"
+include(":app")
+ 
